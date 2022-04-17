b6e4f2712625fae88030965479fa754ffab68f28
Neoalina <neo-alina@yandex.ru>
2022-04-17 22:51:07 +0300

    Created page

---
+++ react-app/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+
+  labels: ['Bakery products, Vegetable oil, Whole-grain, Porridge, Pasta', 'Vegetables, Green-stuff', 'Fruits, Berries', 'Dairy, Mushrooms, Nuts', 'Meat, Fish, Chicken', 'Desserts, Fats'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [41, 19, 15, 11, 11, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function App() {
+  return <Pie data={data} 
+  width={500}
+  height={500}
+  options={{ maintainAspectRatio: false }} />;
+}
+
+export default App;
